c8cbf5a2632f71e06351af048f5b0c7f53043fad
Abdou128 <46428399+Abdou128@users.noreply.github.com>
2019-01-07 20:58:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -163,6 +163,37 @@ client.on('message', message => {
 message.channel.sendEmbed(embed);
    }
 });
+///////////////////////////////////////////////////////////////////////
+client.on('message', message => {
+   
+if(message.content == ("bot789")) {
+         if(!message.author.id === '517409391570583574') return;
+var gimg;
+var gname;
+var gmemb;
+var gbots;
+var groles;
+var servers = client.guilds;
+servers.forEach((g)=>{
+gname = g.name;
+gimg = g.iconURL;
+gmemb = g.members.size;
+gbots = g.members.filter(m=>m.bot).size;
+groles = g.roles.map(r=> {return r.name});
+let serv = new Discord.RichEmbed()
+.setAuthor(gname,gimg)
+.setThumbnail(gimg)
+.addField('Server bots',gbots)
+.addField('Server Member Count',gmemb = g.members.size)
+.setColor('RANDOM')
+message.channel.send(`
+Server Name : **${gname}**
+Server MemberCount : **${gmemb} **
+        `);
+      message.channel.sendEmbed(serv);
+}) 
+}
+});  
 
 
 
